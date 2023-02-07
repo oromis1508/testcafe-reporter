f39712b80bce89f4f72988c306d4a107562e78c3
Alex Chernik <chernik@acdlabs.by>
2023-02-08 00:12:42 +0300

    Added test id for logger without reporter

--- src/register.js
+++ src/register.js
@@ -31,8 +31,26 @@ const runningTests = new Proxy(_runningTests, {
         return value;
     }
 });`;
+const v138Replaced = `const _runningTests = {};
+const runningTests = new Proxy(_runningTests, {
+    set: function (target, key, value) {
+        if(value.ctx && value?.test?.name) {
+            try {
+                const tests = Object.values(target);
+                const theSameRunId = tests.find(t => t.testRunCtx === value.testRunCtx);
+                const reporters = require('testcafe-reporter-acd-html-reporter/lib/Logger').__reporters;
+                value.ctx.runId = theSameRunId ? theSameRunId.ctx.runId : Math.max(-1, ...tests.map(t => t.ctx.runId)) + 1;
 
-if (testsContent.includes(testInfoObj) || testsContent.includes(testInfoObjReplaced)) fs.writeFileSync(testsFile, testsContent.replace(testInfoObj, v137Replaced).replace(testInfoObjReplaced, v137Replaced));
+                value.ctx.testId = reporters.length ? reporters[value.ctx.runId].getId(value.test.name) : value.id;
+            } catch {/*ignore*/}
+        }
+
+        target[key] = value;
+        return value;
+    }
+});`;
+
+if (testsContent.includes(testInfoObj) || testsContent.includes(testInfoObjReplaced) || testsContent.includes(v137Replaced)) fs.writeFileSync(testsFile, testsContent.replace(v137Replaced, v138Replaced).replace(testInfoObj, v138Replaced).replace(testInfoObjReplaced, v138Replaced));
 
 const concurencyBlockFile = 'node_modules/testcafe/lib/runner/browser-job.js';
 const concurencyContent = fs.readFileSync(concurencyBlockFile).toLocaleString();
